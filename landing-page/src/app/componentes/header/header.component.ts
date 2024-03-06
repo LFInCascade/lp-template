5519ebbb3e29168a00c39cd3522917a941ced182
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-03-06 13:34:25 -0300

    refactor :recycle: mudando tamanho do resize

--- landing-page/src/app/componentes/header/header.component.ts
+++ landing-page/src/app/componentes/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,7 +12,20 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   isMenuVisible = false;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.updateMenuVisibility();
+  }
+
   toggleMenu() {
       this.isMenuVisible = !this.isMenuVisible;
   }
-}
+
+  private updateMenuVisibility(): void {
+    // Defina a condição para mostrar ou ocultar o menu com base no tamanho da tela
+    const shouldShowMenu = window.innerWidth <= 768; // Use o mesmo valor que você usa na sua mixin mq(xs)
+
+    if (shouldShowMenu) {
+      this.isMenuVisible = false; // Esconda o menu quando a tela for aumentada
+    }
+}}
