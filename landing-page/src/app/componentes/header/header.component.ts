fe2a5655a760ae18716e08a8e0ea41fe7ba765dc
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-03-06 11:52:33 -0300

    feat :sparkles: adicionando função para ocultar nav

--- landing-page/src/app/componentes/header/header.component.ts
+++ landing-page/src/app/componentes/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: '../../styles/index.scss'
+  
 })
 export class HeaderComponent {
+  isMenuVisible = false;
 
+  toggleMenu() {
+      this.isMenuVisible = !this.isMenuVisible;
+  }
 }
