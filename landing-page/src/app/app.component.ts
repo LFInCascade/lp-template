254994481ed26f1b7de27ae8a7e4068da5ee5da1
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-03-07 16:04:40 -0300

    feat :sparkles: criando seção contacts

--- landing-page/src/app/app.component.ts
+++ landing-page/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componentes/header/header.component';
 import { HeroComponent} from './pages/hero/hero.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, ContactsComponent],
   templateUrl: './app.component.html',
   styleUrl: './styles/index.scss'
 })
